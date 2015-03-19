2611648fe21020e5043d4bbe51f635442a253d21
karina <k.sueminha@gmail.com>
2015-03-19 15:52:16 -0300

    não precisa verificar se as arvores separadas do conjunto B estão ligadas -- não altera o corte

--- task2-lemma3.c
+++ task2-lemma3.c
@@ -40,6 +40,14 @@ void Algorithm1(Graph *G, float c, int m, int root)
 	while(lenB < c*m)
 	{
 		lenCut += lemma2(G, m - lenB, root);
+		/* E se essa nova mini-árvore tiver um verice que é pai de algum dos vertices do conjunto B anterior?
+		Nesse caso, o corte seria menor do que o proposto acima!
+
+		Porem isso não pode acontecer, pois o conjunto W se mantem conexo o tempo todo, e sabemos que se não pegamos 
+		o pai da raiz da mini arvore inclusa em B, é pq todo aquele conjunto tem mais que m vertices.
+		e como W sempre se mantem conexo, se pegarmos algum pai do que já foi escolhido, o conjunto B terá mais que
+		m elementos. */
+		
 		childNumber(G, root, childs, root);
 		lenB = G->V - childs[root];
 		printf("\n");

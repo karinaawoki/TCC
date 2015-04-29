a03d9cc11ee3824af5cefbdf8e64f569c8ba993c
karina <k.sueminha@gmail.com>
2015-04-29 01:46:11 -0300

    adicionando lema 6

---
+++ task5.c
@@ -0,0 +1,25 @@
+#include "common/theorem4.h"
+
+
+int main(int argc, char *argv[])
+{
+	/* argv[1] = filename */
+	/* argv[2] = m        */
+
+	int *B;
+	Graph *G;
+	G = read(argv[1]);
+	B = malloc(G->V*sizeof(int));
+	theorem6(G, B, atoi(argv[2]));
+
+	return 0;
+}
+
+
+void theorem6(Graph *G, int *B, int m)
+{
+	while(Blength < m)
+	{
+		theorem4(G, B, m);
+	}
+}

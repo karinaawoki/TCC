ed7a612fac2f4a6598a1a2e653193375c72d6060
Karina Awoki <k.sueminha@hotmail.com>
2015-05-20 08:35:47 -0300

    task4.c compilando

--- task4.c
+++ task4.c
@@ -6,26 +6,35 @@ int main(int argc, char *argv[])
 	/* argv[1] = filename */
 	/* argv[2] = m        */
 
-	int root, S;
+	int root, S, max;
+	int *maxPath, *labelVec, *r, *index;
 	Graph *G;
 	G = read(argv[1]);
 	setBInit(G);
 	Blength = 0;
 
 	root = (int)(G->V*1.0*rand()/RAND_MAX); 	
+	
+	r = malloc(G->V*sizeof(int));
+	maxPath = maximumPath(G, root);
 
-	/* TODO: take off */
-	root = 21;
 
-	S = theorem4(G, atoi(argv[2]), root);
+	/* Label */
+	max = changeOrderAtAdj(G, maxPath, r);
+	labelVec = label(G, maxPath, max, r);
+
+	index = labelToIndex(G, labelVec);
+
+	S = theorem4(G, atoi(argv[2]), root, labelVec, 
+		index, maxPath, r);
 
 	printf("///////--- %d\n", S);
 	freeGraph(G);
+	free(r);
+	free(maxPath);
+	free(index);
+	free(labelVec);
 	free(setB);
 	
 	return 0;
 }
-
-
-/* TODO: Fazer uma varredura na árvore e ir deletando as arestas de baixo para cima
-- das raízes que não pertencem a P --  */

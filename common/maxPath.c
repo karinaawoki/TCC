2cd1d070278e6200b5f85787341e23bb59941432
karina <k.sueminha@gmail.com>
2015-06-03 22:45:07 -0300

    maxPath modificado para achar caminho de florestas - adiciona árvores! by Cris

---
+++ common/maxPath.c
@@ -0,0 +1,85 @@
+#include "maxPath.h"
+
+int fartherVertex(Graph *G, int vertex, int *parents, int* visited);
+void printPath(int *parents, int init, int end, int *maxPath, int *i);
+
+int *maximumPath(Graph *G)
+{
+  /* The result will be a vector with the maximum path - the number -1 indicate the end of the path */
+  int *x, *y, t, w, *parents, *visited, *maxPath, i, r;
+  
+  visited = malloc(G->V*sizeof(int));
+  parents = malloc(G->V*sizeof(int));
+  maxPath = malloc(G->V*sizeof(int));
+  x = malloc(G->V*sizeof(int));
+  y = malloc(G->V*sizeof(int));
+  r = t = w = 0; 
+  
+  for(i = 0; i<G->V; i++) visited[i] = 0;
+  for(i = 0; i<G->V; i++) maxPath[i] = -1;
+  
+  while (r < G->V) {
+    printf("%d ------ r \n", r); 
+    x[t] = fartherVertex(G, r, parents, visited);
+    y[t] = fartherVertex(G, x[t], parents, visited);
+    printPath(parents, x[t], y[t], maxPath, &w);
+    t++; 
+    
+    r = 0;
+    while ((r < G->V) && visited[r]) r++;
+  }
+  
+  for (i=1; i<t; i++) includeNotOriginalEdges(G, y[i-1], x[i]);
+  
+  free(parents); free(x); free(y);
+  printf("comprimento: %d\n", maxPathLength);
+  return maxPath;
+}
+
+void printPath(int *parents, int init, int end, int *maxPath, int *i)
+{
+  int vertex;
+  
+  printf("TASK 1 - The longest path in the tree is: \n");
+  printf("%d  ", end);
+  maxPath[*i] = end; 
+  (*i)++;
+  vertex = parents[end];
+  while(vertex != init) {
+    printf("%d  ", vertex);
+    maxPath[*i] = vertex;
+    vertex = parents[vertex];
+    (*i)++;
+  }
+  printf("%d\n\n", init);
+  maxPath[*i] = init;
+  maxPathLength = (*i)+1;
+  maxPath[(*i)+1] = -1;
+}
+
+int fartherVertex(Graph *G, int vertex, int *parents, int *visited)
+{ /* QUEUE of vertex - farther at the end */
+  Queue *q;
+  int i, farther = -1;
+  Vertex *aux;
+
+  q = initQueue(vertex);
+  for(i = 0; i<G->V; i++) parents[i] = -1;
+  parents[vertex] = vertex;
+  while(!emptyQueue(q)) {
+    for(aux = G->adj[q->init->num]->next; aux!=NULL; aux = aux->next) {
+      if (parents[aux->vertex]==-1 && aux->edge==1) {
+	insertQueue(q, aux->vertex);
+	parents[aux->vertex] = q->init->num;
+      }
+    }
+    farther = removeQueue(q);
+  }
+
+  for(i = 0; i<G->V; i++) 
+    if (parents[i] != -1) 
+      visited[i] = 1;
+
+  freeQueue(q);
+  return farther;
+}

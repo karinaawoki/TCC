50612d9b0a07b206a42ffd018fc2dabc2267694f
karina <k.sueminha@gmail.com>
2015-06-05 00:34:53 -0300

    concertando a parte em que ele retirava o ultimo elemento de maxPath e arrumando a parte em q ele contava 2 quando o fim = começo

--- common/maxPath.c
+++ common/maxPath.c
@@ -19,7 +19,7 @@ int *maximumPath(Graph *G)
     for(i = 0; i<G->V; i++) maxPath[i] = -1;
     
     while (r < G->V) {
-        if(DEBUG_2)printf("%d ------ r \n", r); 
+        if(DEBUG_2)printf("root: %d \n", r); 
         /* defining the start and the end of the 
            maximumPath of the analized tree */
         x[tree] = fartherVertex(G, r, parents, visited);
@@ -38,7 +38,7 @@ int *maximumPath(Graph *G)
     free(parents); 
     free(x); 
     free(y);
-    if(DEBUG_2) printf("comprimento: %d\n", maxPathLength);
+    if(DEBUG_2||STEP) printf("length: %d\n", maxPathLength);
     return maxPath;
 }
 
@@ -46,22 +46,28 @@ void printPath(int *parents, int init, int end, int *maxPath, int *i)
 {
     int vertex;
     
-    if(DEBUG_2) printf("TASK 1 - The longest path in the tree is: \n");
-    if(DEBUG_2) printf("%d  ", end);
+    if(DEBUG_2||STEP) printf("TASK 1 - The longest path in the tree is: \n");
+    if(DEBUG_2||STEP) printf("%d  ", end);
     maxPath[*i] = end; 
     (*i)++;
     vertex = parents[end];
     while(vertex != init) 
     {
-        if(DEBUG_2) printf("%d  ", vertex);
+        if(DEBUG_2||STEP) printf("%d  ", vertex);
         maxPath[*i] = vertex;
         vertex = parents[vertex];
         (*i)++;
     }
-    if(DEBUG_2) printf("%d\n\n", init);
-    maxPath[*i] = init;
-    maxPathLength = (*i)+1;
-    maxPath[(*i)+1] = -1;
+    if(init!=end)
+    {
+        if(DEBUG_2||STEP) printf("%d\n", init);
+        maxPath[*i] = init;
+        (*i)++;
+    }
+    else if(DEBUG_2||STEP) printf("\n");
+    
+    maxPathLength = (*i);
+
 }
 
 int fartherVertex(Graph *G, int vertex, int *parents, int *visited)

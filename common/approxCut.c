09c9f4a18f012e86bfa50ea939b66a32f6a34126
karina <k.sueminha@gmail.com>
2016-01-20 04:42:18 -0200

    organizando funçõe sdo approx cut - falta organizar a ultima parte

--- common/approxCut.c
+++ common/approxCut.c
@@ -17,12 +17,8 @@ void approxCut(Graph *G, int m, float c, int root, int leftNeighbor, int rightNe
 	NumVert = childs[root];
 
 	if(STEP) printf("    size of the tree rooted at r: %d\n\n", NumVert);
-
-	if (m == NumVert || m == 0 || c <= 0.5)
-		printf("    Number of edges cut: %d\n", simpleApproxCut(G, m, root, NumVert, leftNeighbor, rightNeighbor));
-	else
-		/* c > 1/2 */
-		Algorithm3(G, c, m, root, NumVert, leftNeighbor, rightNeighbor);
+	
+	Algorithm3(G, c, m, root, NumVert, leftNeighbor, rightNeighbor);
 	
 	free(childs);
 }
@@ -30,18 +26,28 @@ void approxCut(Graph *G, int m, float c, int root, int leftNeighbor, int rightNe
 
 void Algorithm3(Graph *G, float c, int m, int root, int NumVert, int leftNeighbor, int rightNeighbor)
 {
-	int sizeB = 0;
 	int edgesCut = 0, oldBLength = Blength;
 
 	if(STEP) printf("    Algorithm 1 - root: %d\n", root);
 	if(STEP) printf("      Initial size of B: %d\n", Blength);
 
-	while(sizeB < c*m)
+	if(m == 0);
+	else if (m == NumVert || c <= 0.5)
 	{
-		if (DEBUG||STEP) printf("      Inside the loop of Algorithm 1.\n");
-		edgesCut += simpleApproxCut(G, m - sizeB, root, NumVert, leftNeighbor, rightNeighbor);
-		
-		sizeB = Blength - oldBLength;
+		printf("    Number of edges cut: %d\n", Algorithm2(G, m, root, NumVert, leftNeighbor, rightNeighbor));
+	}
+	
+	/* c > 1/2 */
+	else
+	{
+		int sizeB = 0;
+		while(sizeB < c*m)
+		{
+			if (DEBUG||STEP) printf("      Inside the loop of Algorithm 1.\n");
+			edgesCut += Algorithm2(G, m - sizeB, root, NumVert, leftNeighbor, rightNeighbor);
+			
+			sizeB = Blength - oldBLength;
+		}
 	}
 	if(STEP) printf("      Final size of B: %d\n", Blength);
 	if(STEP) printf("    Algorithm 1 - number of edges cut: %d\n", edgesCut);
